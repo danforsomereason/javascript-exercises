8381b53cadf739f4ad69aeec3e6f5c0b6c495ccf
danforsomereason <147667936+danforsomereason@users.noreply.github.com>
2025-04-20 17:10:34 -0400

    calculator practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+       let newArray = array.filter(item => !args.includes(item))
+       return newArray;
+}
+
+
+console.log(removeFromArray([1, 2, 3, 4], null, 1));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(rest) {
+  let total = 1;
+  for(let i = 0; i < rest.length; i++){
+    total *= rest[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for(i = num; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
